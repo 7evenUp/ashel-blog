1404313f9abaf4a863fa149130319691872dcbdb
Artyom <sheludeshev.artyom@mail.ru>
2022-09-14 15:26:20 +0300

    Initial stylings

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="w-full flex items-center justify-center border-b border-slate-700 py-4 sticky top-0">
+      <h2 className="text-xl text-slate-700">Header</h2>
+    </div>
+  )
+}
