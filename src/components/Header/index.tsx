bc1586e84e38a9c9c8875dc011bdac076ea36c56
Artyom <sheludeshev.artyom@mail.ru>
2022-09-15 17:41:34 +0300

    Made Link show in UI when it is active

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,25 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router';
 import React from 'react'
 
 export default function Header() {
+  const router = useRouter();
+  console.log(router.pathname)
+
   return (
     <div className="w-full flex items-center justify-center border-b border-slate-700 py-4 sticky top-0">
-      <h2 className="text-xl text-slate-700">Header</h2>
-      <ul>
-        <li>
+      <ul className="flex gap-10">
+        <li className={router.pathname === '/' ? 'font-semibold' : ''}>
+          <Link href="/">
+            <a>Обо мне</a>
+          </Link>
+        </li>
+        <li className={router.pathname === '/blog' ? 'font-semibold' : ''}>
           <Link href="/blog">
             <a>Блог</a>
           </Link>
         </li>
-        <li>
+        <li className={router.pathname === '/gallery' ? 'font-semibold' : ''}>
           <Link href="/gallery">
             <a>Фотокарточки</a>
           </Link>
