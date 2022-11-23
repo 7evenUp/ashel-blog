f484a72d59a16508970817478e06f8ed2cd88d93
Artyom <sheludeshev.artyom@mail.ru>
2022-11-24 00:09:24 +0300

    Fetch posts on blog page

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -3,12 +3,41 @@ import { trpc } from "../../utils/trpc";
 
 const Blog: NextPage = () => {
   const x = trpc.useQuery(["example.hello"]);
+  const {
+    data: posts,
+    isLoading,
+    error,
+    isSuccess,
+  } = trpc.useQuery(["posts.getPublished"]);
+
+  if (isLoading) {
+    return (
+      <h1 className="text-[5rem] leading-normal font-extrabold text-white drop-shadow-[0_0_1px_rgb(0,0,0)]">
+        Загрузка постов
+      </h1>
+    );
+  }
+
+  if (error) {
+    return (
+      <h1 className="text-[5rem] leading-normal font-extrabold text-white drop-shadow-[0_0_1px_rgb(0,0,0)]">
+        Ошибка: {error.message}
+      </h1>
+    );
+  }
 
   return (
     <>
       <h1 className="text-[5rem] leading-normal font-extrabold text-white drop-shadow-[0_0_1px_rgb(0,0,0)]">
         Это мой блог
       </h1>
+      <div>
+        {isSuccess && (
+          posts.map(post => (
+            <div key={post.id}>{new Date(post.createdAt).toDateString()}</div>
+          ))
+        )}
+      </div>
     </>
   );
 };
