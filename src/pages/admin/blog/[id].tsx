d8dac1e540266839451a66b18c6ebc2ea3be5fd6
Artyom <sheludeshev.artyom@mail.ru>
2022-12-17 01:58:20 +0300

    Making modal component for description

--- src/pages/admin/blog/[id].tsx
+++ src/pages/admin/blog/[id].tsx
@@ -8,7 +8,7 @@ import { prisma } from "../../../server/db/client";
 import { trpc } from "../../../utils/trpc";
 import FloatButtons from "./components/FloatButtons";
 import Modal from "./components/Modal";
-import Editor from './Editor'
+import Editor from "./Editor";
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const posts = await prisma.post.findMany({
@@ -38,7 +38,9 @@ export const getStaticProps = async (
         props: {
           post: {
             ...post,
-            publishedAt: post.publishedAt ? new Date(post.publishedAt).toLocaleDateString() : null,
+            publishedAt: post.publishedAt
+              ? new Date(post.publishedAt).toLocaleDateString()
+              : null,
             createdAt: new Date(post.createdAt).toLocaleDateString(),
           },
         },
@@ -58,44 +60,20 @@ const Post = ({
   error,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const [title, setTitle] = useState(post?.title || "");
-  const [description, setDescription] = useState(post?.desc || "")
+  const [description, setDescription] = useState(post?.desc || "");
   const [editorState, setEditorState] = useState(post?.content || "");
-  const [modalOpened, setModalOpened] = useState(false)
-  const saveMutation = trpc.useMutation("posts.update")
-  const publishMutation = trpc.useMutation("posts.publish")
+  const [isModalOpened, setIsModalOpened] = useState(false);
+  const saveMutation = trpc.useMutation("posts.update");
+  const publishMutation = trpc.useMutation("posts.publish");
 
-  const changeModalStatus = () => setModalOpened(!modalOpened)
+  const changeModalStatus = () => setIsModalOpened(!isModalOpened);
 
   return (
-    <div className="flex flex-col gap-2">
-      {error && <h1>Error occured!</h1>}
+    <>
+      <div className="flex flex-col gap-2">
+        {error && <h1>Error occured!</h1>}
 
-      {post !== undefined && (
-        <>
-          <Modal />
-          <FloatButtons
-            states={{ title, desc: description, content: editorState }}
-            handlePublish={async () => {
-              await publishMutation.mutateAsync({
-                id: post.id,
-                title,
-                desc: 'This is description for published posts',
-                content: editorState || ''
-              }, {
-                onSuccess: data => console.log(data)
-              })
-            }}
-            handleSave={async () => {
-              await saveMutation.mutateAsync({
-                id: post.id,
-                title,
-                desc: 'This is a custom description',
-                content: editorState || ''
-              }, {
-                onSuccess: (data) => { console.log(data) }
-              })
-            }}
-          />
+        {post !== undefined && (
           <div className="flex flex-col">
             <div className="flex items-end">
               <span>Created at: {post.createdAt}</span>
@@ -106,26 +84,64 @@ const Post = ({
                 value={title}
                 onChange={(e) => setTitle(e.currentTarget.value)}
               />
-              
+
               <button
                 className="ml-auto hover:bg-beige transition-all rounded-lg p-1"
                 type="button"
                 onClick={changeModalStatus}
               >
-                <svg className="w-8 h-8 fill-black" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                <svg
+                  className="w-8 h-8 fill-black"
+                  viewBox="0 0 40 40"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
                   <path d="M12.4999 25H14.9999V18.3333H12.4999V20.4167H9.99992V22.9167H12.4999V25ZM16.6666 22.9167H29.9999V20.4167H16.6666V22.9167ZM24.9999 18.3333H27.4999V16.25H29.9999V13.75H27.4999V11.6667H24.9999V18.3333ZM9.99992 16.25H23.3333V13.75H9.99992V16.25ZM13.3333 35V31.6667H6.66659C5.74992 31.6667 4.96547 31.3406 4.31325 30.6883C3.65992 30.035 3.33325 29.25 3.33325 28.3333V8.33333C3.33325 7.41667 3.65992 6.63167 4.31325 5.97833C4.96547 5.32611 5.74992 5 6.66659 5H33.3333C34.2499 5 35.0349 5.32611 35.6883 5.97833C36.3405 6.63167 36.6666 7.41667 36.6666 8.33333V28.3333C36.6666 29.25 36.3405 30.035 35.6883 30.6883C35.0349 31.3406 34.2499 31.6667 33.3333 31.6667H26.6666V35H13.3333ZM6.66659 28.3333H33.3333V8.33333H6.66659V28.3333ZM6.66659 28.3333V8.33333V28.3333Z" />
                 </svg>
               </button>
-              
             </div>
-            
+
             <div className="editor-shell">
               <Editor state={editorState} setState={setEditorState} />
             </div>
           </div>
-        </>
+        )}
+      </div>
+
+      {isModalOpened && <Modal state={description} setState={setDescription} close={changeModalStatus} />}
+      {post !== undefined && (
+        <FloatButtons
+          states={{ title, desc: description, content: editorState }}
+          handlePublish={async () => {
+            await publishMutation.mutateAsync(
+              {
+                id: post.id,
+                title,
+                desc: "This is description for published posts",
+                content: editorState || "",
+              },
+              {
+                onSuccess: (data) => console.log(data),
+              }
+            );
+          }}
+          handleSave={async () => {
+            await saveMutation.mutateAsync(
+              {
+                id: post.id,
+                title,
+                desc: "This is a custom description",
+                content: editorState || "",
+              },
+              {
+                onSuccess: (data) => {
+                  console.log(data);
+                },
+              }
+            );
+          }}
+        />
       )}
-    </div>
+    </>
   );
 };
 
--- src/pages/admin/blog/components/Modal.tsx
+++ src/pages/admin/blog/components/Modal.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { Dispatch, SetStateAction } from "react";
 
-const Modal = () => {
+const Modal = ({
+  state,
+  setState,
+  close
+}: {
+  state: string;
+  setState: Dispatch<SetStateAction<string>>;
+  close: () => void
+}) => {
   return (
-    <div>Modal</div>
+    <div className="absolute w-screen h-screen bg-black opacity-30 top-0 left-0 z-20">
+      <div className="flex flex-col gap-2 absolute top-1/2 left-1/2 w-96 h-80 -translate-x-1/2 -translate-y-1/2 z-30">
+        <span className="text-white text-lg">Description: </span>
+        <textarea
+          className="text-black bg-white w-full h-full outline-none p-2 resize-none"
+          value={state}
+          onChange={(e) => setState(e.currentTarget.value)}
+        />
+      </div>
+      <button
+        type="button"
+        className="absolute right-4 top-4 z-30 text-white text-5xl rotate-45 hover:text-beige"
+        onClick={close}
+      >+</button>
+    </div>
   )
-}
+};
 
-export default Modal
+export default Modal;
