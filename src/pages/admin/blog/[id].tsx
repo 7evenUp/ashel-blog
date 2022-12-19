06bee436a0f20a5ffcd95a6ce044105a86a84c3b
Artyom <sheludeshev.artyom@mail.ru>
2022-12-19 16:45:41 +0300

    Made saving editor state on change instead of clicking a button

--- src/pages/admin/blog/[id].tsx
+++ src/pages/admin/blog/[id].tsx
@@ -70,42 +70,40 @@ const Post = ({
 
   return (
     <>
-      <div className="flex flex-col gap-2">
-        {error && <h1>Error occured!</h1>}
+      {error && <h1>Error occured!</h1>}
 
-        {post !== undefined && (
-          <div className="flex flex-col">
-            <div className="flex items-end">
-              <span>Created at: {post.createdAt}</span>
-              <input
-                className="text-3xl w-full outline-none text-center border-b-2 bg-white text-black placeholder:text-beige pb-2"
-                type="text"
-                placeholder="Title of your story"
-                value={title}
-                onChange={(e) => setTitle(e.currentTarget.value)}
-              />
+      {post !== undefined && (
+        <div className="flex flex-col gap-2 w-full">
+          <div className="flex items-end">
+            <span>Created at: {post.createdAt}</span>
+            <input
+              className="text-3xl w-full outline-none text-center border-b-2 bg-white text-black placeholder:text-beige pb-2"
+              type="text"
+              placeholder="Title of your story"
+              value={title}
+              onChange={(e) => setTitle(e.currentTarget.value)}
+            />
 
-              <button
-                className="hover:bg-beige transition-all rounded-lg p-1"
-                type="button"
-                onClick={changeModalStatus}
+            <button
+              className="hover:bg-beige transition-all rounded-lg p-1"
+              type="button"
+              onClick={changeModalStatus}
+            >
+              <svg
+                className="w-8 h-8 fill-black"
+                viewBox="0 0 40 40"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <svg
-                  className="w-8 h-8 fill-black"
-                  viewBox="0 0 40 40"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M12.4999 25H14.9999V18.3333H12.4999V20.4167H9.99992V22.9167H12.4999V25ZM16.6666 22.9167H29.9999V20.4167H16.6666V22.9167ZM24.9999 18.3333H27.4999V16.25H29.9999V13.75H27.4999V11.6667H24.9999V18.3333ZM9.99992 16.25H23.3333V13.75H9.99992V16.25ZM13.3333 35V31.6667H6.66659C5.74992 31.6667 4.96547 31.3406 4.31325 30.6883C3.65992 30.035 3.33325 29.25 3.33325 28.3333V8.33333C3.33325 7.41667 3.65992 6.63167 4.31325 5.97833C4.96547 5.32611 5.74992 5 6.66659 5H33.3333C34.2499 5 35.0349 5.32611 35.6883 5.97833C36.3405 6.63167 36.6666 7.41667 36.6666 8.33333V28.3333C36.6666 29.25 36.3405 30.035 35.6883 30.6883C35.0349 31.3406 34.2499 31.6667 33.3333 31.6667H26.6666V35H13.3333ZM6.66659 28.3333H33.3333V8.33333H6.66659V28.3333ZM6.66659 28.3333V8.33333V28.3333Z" />
-                </svg>
-              </button>
-            </div>
-
-            <div className="editor-shell">
-              <Editor state={editorState} setState={setEditorState} />
-            </div>
+                <path d="M12.4999 25H14.9999V18.3333H12.4999V20.4167H9.99992V22.9167H12.4999V25ZM16.6666 22.9167H29.9999V20.4167H16.6666V22.9167ZM24.9999 18.3333H27.4999V16.25H29.9999V13.75H27.4999V11.6667H24.9999V18.3333ZM9.99992 16.25H23.3333V13.75H9.99992V16.25ZM13.3333 35V31.6667H6.66659C5.74992 31.6667 4.96547 31.3406 4.31325 30.6883C3.65992 30.035 3.33325 29.25 3.33325 28.3333V8.33333C3.33325 7.41667 3.65992 6.63167 4.31325 5.97833C4.96547 5.32611 5.74992 5 6.66659 5H33.3333C34.2499 5 35.0349 5.32611 35.6883 5.97833C36.3405 6.63167 36.6666 7.41667 36.6666 8.33333V28.3333C36.6666 29.25 36.3405 30.035 35.6883 30.6883C35.0349 31.3406 34.2499 31.6667 33.3333 31.6667H26.6666V35H13.3333ZM6.66659 28.3333H33.3333V8.33333H6.66659V28.3333ZM6.66659 28.3333V8.33333V28.3333Z" />
+              </svg>
+            </button>
           </div>
-        )}
-      </div>
+
+          <div className="editor-shell">
+            <Editor state={editorState} setState={setEditorState} />
+          </div>
+        </div>
+      )}
 
       {isModalOpened && <Modal state={description} setState={setDescription} close={changeModalStatus} />}
       {post !== undefined && (
