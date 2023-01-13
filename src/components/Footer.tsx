9059a5a6a4855aa8f49a743cbdfae0036a77ebf6
Artyom <sheludeshev.artyom@mail.ru>
2023-01-13 19:06:30 +0300

    Added copyrights to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className="w-full h-[200px] bg-black text-white pb-4 flex items-end justify-center">
+    <footer className="w-full h-[200px] bg-black text-white pb-4 flex items-center justify-center flex-col">
+      <span className="text-black">&#169; 2022-2023, Artyom Sheludeshev</span>
       <span>не вижу смысла писать копирайт</span>
     </footer>
   )
