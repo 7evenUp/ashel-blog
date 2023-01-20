3f2a996c3f8662220c3883c07aa5ad1430d66925
Artyom <sheludeshev.artyom@mail.ru>
2023-01-20 04:58:50 +0300

    Made available photos deletion

--- src/pages/admin/gallery/index.tsx
+++ src/pages/admin/gallery/index.tsx
@@ -1,22 +1,47 @@
 import type { GetServerSideProps, NextPage } from "next";
 import Image from "next/image";
+import { useState } from "react";
+import { GalleryImage } from "../../../components";
 import { getServerAuthSession } from "../../../server/common/get-server-auth-session";
 import { supabase } from "../../../supabase/supabaseClient";
 import { trpc } from "../../../utils/trpc";
 
 const Gallery: NextPage = () => {
-  const createMutation = trpc.useMutation('gallery.create')
+  const createMutation = trpc.useMutation("gallery.create");
+  const deleteMutation = trpc.useMutation("gallery.delete");
+  const { data, error, isLoading, refetch } = trpc.useQuery(["gallery.getAll"]);
+
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const handleDelete = async (id: number, path: string) => {
+    const { data, error } = await supabase.storage
+      .from("photos")
+      .remove([path]);
+
+    if (error) console.error(error);
+    else {
+      await deleteMutation.mutateAsync(
+        { id },
+        {
+          onSuccess: (data) => {
+            setIsDeleting(false)
+            console.log("DATA AFTER DELETION:", data);
+          },
+        }
+      );
+    }
+  };
 
   const onSelectFile = async (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files === null) return;
-    const file = e.target.files[0]; 
+    const file = e.target.files[0];
 
     if (file === undefined) return;
 
     if (file.type && file.type.startsWith("image/")) {
-      const folderName = 'gallery'
+      const folderName = "gallery";
       const [fileName, ...rest] = file.name.split(".");
-      const fileExt = rest[rest.length - 1]
+      const fileExt = rest[rest.length - 1];
       const filePath = `${folderName}/${fileName}.${fileExt}`;
 
       const { data: uploadData, error } = await supabase.storage
@@ -25,12 +50,18 @@ const Gallery: NextPage = () => {
 
       if (error) console.error(error);
       else {
-        await createMutation.mutateAsync({
-          title: '123',
-          src: uploadData.path
-        }, {
-          onSuccess: data => console.log('Success upload:', data)
-        })
+        await createMutation.mutateAsync(
+          {
+            title: "123",
+            src: uploadData.path,
+          },
+          {
+            onSuccess: (data) => {
+              refetch();
+              console.log("Success upload:", data);
+            },
+          }
+        );
       }
     }
   };
@@ -44,42 +75,31 @@ const Gallery: NextPage = () => {
 
         <label className="flex gap-4 items-center">
           <span>Load Image</span>
-          <input type="file" accept="image/*" onChange={onSelectFile} required />
+          <input
+            type="file"
+            accept="image/*"
+            onChange={onSelectFile}
+            required
+          />
         </label>
       </div>
 
       <div className="grid gap-2 lg:gap-4 w-full lg:grid-cols-2 2xl:grid-cols-3 mt-5">
-        {[
-          "https://images.unsplash.com/photo-1671726805766-de50e4327182?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-          "https://images.unsplash.com/photo-1673537074513-e66435b69012?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
-          "https://images.unsplash.com/photo-1673363006135-b298da87a3de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=1000&q=60",
-          "https://images.unsplash.com/photo-1673546803288-488dbba044c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=1000&q=60",
-          "https://images.unsplash.com/photo-1673557143243-39f2995a3bca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=1000&q=60",
-          "https://images.unsplash.com/photo-1671725501884-e832fb8d0d84?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=1000&q=60",
-          "https://images.unsplash.com/photo-1673423050335-43b991cf57e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=1000&q=60",
-        ].map((el, key) => {
-          return (
-            <div className="group relative w-full h-72 sm:h-80 md:h-96" key={key}>
-              <Image
-                className="object-cover grayscale-0 group-hover:grayscale group-hover:scale-110 duration-300"
-                src={el}
-                layout={"fill"}
-                sizes="(max-width: 1024px) 100vw,(max-width: 1536px) 50vw,33vw"
-                quality={100}
-              />
+        {data &&
+          data.map((photo) => (
+            <div key={photo.id} className="relative">
+              <GalleryImage path={photo.src} />
               <button
                 onClick={() => {
-                  // setIsDeleting(true)
-                  // handleDelete(post.id)
+                  setIsDeleting(true);
+                  handleDelete(photo.id, photo.src);
                 }}
-                className="absolute right-4 bottom-4 py-1 px-2 rounded-md bg-white hover:bg-black hover:text-white transition-all py-1"
+                className="absolute right-4 bottom-4 py-1 px-2 rounded-md bg-white hover:bg-black hover:text-white transition-all"
               >
-                {/* { isDeleting ? 'Deleting' : 'Delete'} */}
-                Delete
+                {isDeleting ? "Deleting" : "Delete"}
               </button>
             </div>
-          );
-        })}
+          ))}
       </div>
     </>
   );
