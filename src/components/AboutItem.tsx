d9932307093e766191297ec3b6f1a87f045dfa5f
Artyom <sheludeshev.artyom@mail.ru>
2023-02-11 00:11:29 +0300

    Code clearing. Improved types

--- src/components/AboutItem.tsx
+++ src/components/AboutItem.tsx
@@ -1,32 +1,32 @@
-type AboutItemType = {
+export default ({
+  heading,
+  text,
+  link,
+}: {
   heading: string;
-  body: string;
+  text: string;
   link?: string;
-};
-
-export default function AboutItem({ heading, body, link }: AboutItemType) {
-  return (
-    <a
-      href={link}
-      target="_blank"
-      rel="noreferrer"
-      className="group flex justify-between gap-6 md:gap-10 flex-col md:flex-row"
-    >
-      <div className="md:border-b">
-        <h2
-          className={`font-serif text-4xl sm:text-5xl text-white drop-shadow-[0_0_1px_rgb(0,0,0)] md:w-[348px] tracking-wider
+}) => (
+  <a
+    href={link}
+    target="_blank"
+    rel="noreferrer"
+    className="group flex justify-between gap-6 md:gap-10 flex-col md:flex-row"
+  >
+    <div className="md:border-b">
+      <h2
+        className={`font-serif text-4xl sm:text-5xl text-white drop-shadow-[0_0_1px_rgb(0,0,0)] md:w-[348px] tracking-wider
             ${
               link &&
               "group-hover:text-black group-hover:drop-shadow-none transition-all"
             }`}
-        >
-          {heading}
-        </h2>
-      </div>
+      >
+        {heading}
+      </h2>
+    </div>
 
-      <p className="text-lg leading-7 grow shrink border-b xl:w-[700px] pb-4">
-        {body}
-      </p>
-    </a>
-  );
-}
+    <p className="text-lg leading-7 grow shrink border-b xl:w-[700px] pb-4">
+      {text}
+    </p>
+  </a>
+);
