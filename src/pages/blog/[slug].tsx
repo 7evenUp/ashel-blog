6facfeb77b0933fe6423f32026352df555441bf7
Artyom <sheludeshev.artyom@mail.ru>
2023-02-23 19:05:24 +0300

    Added SEO optimizations

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -5,6 +5,7 @@ import { getFileBySlug, getFiles } from "../../lib/mdx";
 import { FrontMatterTypes, MdxSource } from "../../../global";
 import MDXComponents from "../../components/MDXComponents";
 import { dayjs } from "../../lib/dayjs";
+import { NextSeo } from "next-seo";
 
 type Props = {
   frontMatter: FrontMatterTypes;
@@ -35,26 +36,42 @@ export const getStaticProps = async ({
 export default function BlogPost({ mdxSource, frontMatter }: Props) {
   console.log("FRONT MATTER: ", frontMatter);
   return (
-    <div className="flex flex-col items-center mt-16 w-full">
-      <div className="flex flex-col items-center gap-2 mobile:gap-4 mb-8">
-        <h1 className="text-2xl mobile:text-3xl sm:text-4xl md:text-5xl font-serif tracking-wider text-center">
-          {frontMatter.title}
-        </h1>
-        <span className="mobile:text-lg">
-          {dayjs(frontMatter.publishedAt).format("ll")}
-        </span>
-        <span className="text-sm mobile:text-base font-semibold">
-          {Math.ceil(frontMatter.readingTime.minutes)} мин. чтения
-        </span>
+    <>
+      <NextSeo
+        title={`Ashel blog - ${frontMatter.title}`}
+        description={frontMatter.summary}
+        canonical={`https://www.ashel.site/blog/${frontMatter.slug}`}
+        openGraph={{
+          url: `https://www.ashel.site/blog/${frontMatter.slug}`,
+          title: `Ashel blog - ${frontMatter.title}`,
+          description: frontMatter.summary,
+          type: 'article',
+          article: {
+            publishedTime: dayjs(frontMatter.publishedAt).format("ll")
+          }
+        }}
+      />
+      <div className="flex flex-col items-center mt-16 w-full">
+        <div className="flex flex-col items-center gap-2 mobile:gap-4 mb-8">
+          <h1 className="text-2xl mobile:text-3xl sm:text-4xl md:text-5xl font-serif tracking-wider text-center">
+            {frontMatter.title}
+          </h1>
+          <span className="mobile:text-lg">
+            {dayjs(frontMatter.publishedAt).format("ll")}
+          </span>
+          <span className="text-sm mobile:text-base font-semibold">
+            {Math.ceil(frontMatter.readingTime.minutes)} мин. чтения
+          </span>
+        </div>
+        <article className="flex flex-col w-full lg:max-w-[860px]">
+          <MDXRemote
+            {...mdxSource}
+            components={{
+              ...MDXComponents,
+            }}
+          />
+        </article>
       </div>
-      <article className="flex flex-col w-full lg:max-w-[860px]">
-        <MDXRemote
-          {...mdxSource}
-          components={{
-            ...MDXComponents,
-          }}
-        />
-      </article>
-    </div>
+    </>
   );
 }
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -2,6 +2,11 @@ import type { InferGetStaticPropsType } from "next";
 import { Post } from "../../components";
 import { getAllPublishedFilesFrontMatter } from "../../lib/mdx";
 import { groupBy } from "../../lib/groupBy";
+import { NextSeo } from "next-seo";
+
+const url = "https://www.ashel.site/blog"
+const title = "Ashel blog"
+const description = 'Programming tutorials, thoughts on different topics and lifehacks.';
 
 export const getStaticProps = async () => {
   const blogPosts = await getAllPublishedFilesFrontMatter('blog');
@@ -15,9 +20,21 @@ export const getStaticProps = async () => {
 
 const Blog = ({blogPosts, groupedBlogPosts}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
+    <>
+    <NextSeo
+        title={title}
+        description={description}
+        canonical={url}
+        openGraph={{
+          url,
+          title,
+          description,
+        }}
+      />
     <div className="flex flex-col gap-24 mt-16 w-full">
       {blogPosts.map((post) => <Post key={post.id} post={post} />)}
     </div>
+    </>
   )
 };
 
