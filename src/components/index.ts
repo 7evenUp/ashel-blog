7896453ebc03bc6a21719aec87fad682400ff685
Artyom <sheludeshev.artyom@mail.ru>
2023-04-12 18:11:11 +0300

    Fix image

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,5 @@
 export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
-export { default as AboutItem } from "./AboutItem";
-export { default as Contacts } from "./Contacts";
 export { default as Layout } from "./Layout";
 export { default as Post } from "./Post";
 export { default as MDXComponents } from "./MDXComponents";
